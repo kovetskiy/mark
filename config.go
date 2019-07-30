ebe2b3a830a8586363dcd7f8c52105e97d18a0b0
Stanislav Seletskiy <s.seletskiy@gmail.com>
2019-07-31 01:35:17 +0300

    config: use ko

---
+++ config.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/kovetskiy/ko"
+)
+
+type Config struct {
+	Username string `env:"MARK_USERNAME" toml:"username"`
+	Password string `env:"MARK_PASSWORD" toml:"password"`
+	BaseURL  string `env:"MARK_BASE_URL" toml:"base_url"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	config := &Config{}
+	err := ko.Load(path, config)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return config, nil
+		}
+
+		return nil, err
+	}
+
+	return config, nil
+}
