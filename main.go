f338ed90fd2da9c0df2fb88191385faeaf53e6bf
Egor Kovetskiy <e.kovetskiy@gmail.com>
2021-02-03 07:06:35 +0300

    bump to 5.0, new markdown renderer

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ Options:
 )
 
 func main() {
-	args, err := docopt.Parse(usage, nil, true, "4.2", false)
+	args, err := docopt.Parse(usage, nil, true, "5.0", false)
 	if err != nil {
 		panic(err)
 	}
