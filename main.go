398cc7375e40f67b728bc6631a489020999e16b0
Egor Kovetskiy <e.kovetskiy@gmail.com>
2021-02-25 08:07:15 +0300

    5.2.1

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ Options:
 )
 
 func main() {
-	args, err := docopt.Parse(usage, nil, true, "5.2", false)
+	args, err := docopt.Parse(usage, nil, true, "5.2.1", false)
 	if err != nil {
 		panic(err)
 	}
