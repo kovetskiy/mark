7cd0ebd7824d7ba7eeec6aab4da883c0c64676c0
Egor Kovetskiy <e.kovetskiy@gmail.com>
2021-03-17 09:24:58 +0300

    bump to 5.4

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ Options:
 )
 
 func main() {
-	args, err := docopt.Parse(usage, nil, true, "5.2.1", false)
+	args, err := docopt.Parse(usage, nil, true, "5.4", false)
 	if err != nil {
 		panic(err)
 	}
