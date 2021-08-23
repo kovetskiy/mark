4e47ae14a7a4d53ea958549bb4967b94392a8614
Egor Kovetskiy <e.kovetskiy@gmail.com>
2021-08-23 13:20:37 +0600

    bump to 6.2

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ type Flags struct {
 }
 
 const (
-	version = "6.1"
+	version = "6.2"
 	usage   = `mark - a tool for updating Atlassian Confluence pages from markdown.
 
 Docs: https://github.com/kovetskiy/mark
