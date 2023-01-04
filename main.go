ab5407091b8847012952e0f61dcd1787b827946a
Egor Kovetskiy <e.kovetskiy@gmail.com>
2023-01-04 08:59:03 +0000

    bump version
    Signed-off-by: Egor Kovetskiy <e.kovetskiy@gmail.com>

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ type Flags struct {
 }
 
 const (
-	version = "8.5"
+	version = "8.6"
 	usage   = `mark - a tool for updating Atlassian Confluence pages from markdown.
 
 Docs: https://github.com/kovetskiy/mark
