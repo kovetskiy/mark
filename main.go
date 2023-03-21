d00d94afa3bea8aac24e3898d1904cdf056312e8
Manuel Rüger <manuel@rueg.eu>
2023-03-21 11:09:47 +0100

    Bump to version 8.9

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ type Flags struct {
 }
 
 const (
-	version = "8.8"
+	version = "8.9"
 	usage   = `mark - a tool for updating Atlassian Confluence pages from markdown.
 
 Docs: https://github.com/kovetskiy/mark
