0988a7aba19157666c64d7c3d4fd67c19e63e24c
Manuel Rüger <manuel@rueg.eu>
2023-05-24 19:31:08 +0200

    Add Page properties macro

--- pkg/mark/stdlib/stdlib.go
+++ pkg/mark/stdlib/stdlib.go
@@ -357,6 +357,14 @@ func templates(api *confluence.API) (*template.Template, error) {
 			`</ac:structured-macro>`,
 		),
 
+		/* https://confluence.atlassian.com/conf59/page-properties-macro-792499154.html */
+
+		`ac:details`: text(
+			`<ac:structured-macro ac:name="details" ac:schema-version="1"">`,
+			`<ac:rich-text-body>{{ .Body }}</ac:rich-text-body>`,
+			`</ac:structured-macro>`,
+		),
+
 		// TODO(seletskiy): more templates here
 	} {
 		templates, err = templates.New(name).Parse(body)
