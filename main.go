bc5c7ae2e7f58e17be2561b20f570992642c83ed
Manuel Rüger <manuel@rueg.eu>
2023-06-21 18:40:12 +0200

    Bump to v9.6.2

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	version     = "9.6.1"
+	version     = "9.6.2"
 	usage       = "A tool for updating Atlassian Confluence pages from markdown."
 	description = `Mark is a tool to update Atlassian Confluence pages from markdown. Documentation is available here: https://github.com/kovetskiy/mark`
 )
