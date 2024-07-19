a4722fe53ed89194e1baaf83c43169d7846b2ae3
Manuel Rüger <manuel@rueg.eu>
2024-07-19 17:16:33 +0200

    Bump to v9.13.0

--- README.md
+++ README.md
@@ -751,7 +751,7 @@ USAGE:
    mark [global options] 
 
 VERSION:
-   9.12.0
+   9.13.0
 
 DESCRIPTION:
    Mark is a tool to update Atlassian Confluence pages from markdown. Documentation is available here: https://github.com/kovetskiy/mark
--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	version     = "9.12.0"
+	version     = "9.13.0"
 	usage       = "A tool for updating Atlassian Confluence pages from markdown."
 	description = `Mark is a tool to update Atlassian Confluence pages from markdown. Documentation is available here: https://github.com/kovetskiy/mark`
 )
