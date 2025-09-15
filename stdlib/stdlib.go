f08991137e14e4c70aedd757a878c5dc13834f5a
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2025-09-15 15:02:12 +0200

    refactor: check for Server var in template

--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -150,7 +150,9 @@ func templates(api *confluence.API) (*template.Template, error) {
 		`ac:jira:ticket`: text(
 			`<ac:structured-macro ac:name="jira">`,
 			`<ac:parameter ac:name="key">{{ .Ticket }}</ac:parameter>`,
-			`<ac:parameter ac:name="server">{{ or .Server "System JIRA" }}</ac:parameter>`,
+			`{{ if .Server }}`,
+				`<ac:parameter ac:name="server">{{ .Server }}</ac:parameter>`,
+			`{{ end }}`,
 			`</ac:structured-macro>`,
 		),
 
